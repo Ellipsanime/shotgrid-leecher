964b52c87e2b42a91560cb237bc96afdaa7d8c3b
Victor Bartel <victor.bartel@yandex.com>
2021-07-13 17:11:53 +0200

    Add simple shotgrid event connectivity

--- shotgrid_leecher/event_leecher.py
+++ shotgrid_leecher/event_leecher.py
@@ -1,5 +1,38 @@
-from typing import NoReturn
+import os
+
+from toolz import memoize
+import shotgun_api3 as sg
 
 
-async def leech_recent_events() -> NoReturn:
+@memoize
+def _get_shotgrid_connection() -> sg.Shotgun:
+    url = os.getenv("SHOTGRID_URL")
+    login = os.getenv("SHOTGRID_LOGIN")
+    password = os.getenv("SHOTGRID_PASSWORD")
+    return sg.Shotgun(url, login=login, password=password)
+
+
+async def get_recent_events() -> None:
+    connection = _get_shotgrid_connection()
+    filters = [["id", "greater_than", 100]]
+    fields = [
+        "id",
+        "event_type",
+        "attribute_name",
+        "meta",
+        "entity",
+        "user",
+        "project",
+        "session_uuid",
+        "created_at",
+    ]
+    order = [{"column": "id", "direction": "asc"}]
+    events = connection.find(
+        "EventLogEntry",
+        filters,
+        fields,
+        order,
+        limit=100,
+    )
+    print(events)
     pass
--- shotgrid_leecher/main.py
+++ shotgrid_leecher/main.py
@@ -1,18 +1,28 @@
-import event_leecher as event_leecher
-from typing import Any, Dict, NoReturn
+from typing import Any, Dict
+
+import uvicorn as uvicorn
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 
+import event_leecher as event_leecher
 
 app = FastAPI()
 
 
 @app.on_event("startup")
 @repeat_every(seconds=60)
-async def start_leeching_events() -> NoReturn:
-    await event_leecher.leech_recent_events()
+async def start_leeching_events() -> None:
+    await event_leecher.get_recent_events()
 
 
 @app.get("/events")
 async def events(user_id: str) -> Dict[str, Any]:
     return {"id": -1}
+
+
+def start():
+    uvicorn.run(app, host="0.0.0.0", port=9001)
+
+
+if __name__ == "__main__":
+    start()
