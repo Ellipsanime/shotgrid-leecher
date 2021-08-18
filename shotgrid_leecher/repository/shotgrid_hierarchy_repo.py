578f6c9fc1ca3001a57696b0e8c66861ddcb117f
Vic Bartel <vbartel@yanport.com>
2021-08-18 17:59:06 +0200

    Add an initial version of hierarchy unpacker

--- shotgrid_leecher/repository/shotgrid_hierarchy_repo.py
+++ shotgrid_leecher/repository/shotgrid_hierarchy_repo.py
@@ -1,7 +1,10 @@
 import time
-from typing import List, Dict, Any, Set, Tuple
+import uuid
+from itertools import chain, starmap
+from typing import List, Dict, Any, Set, Tuple, Iterable
 
 import shotgun_api3 as sg
+from toolz import curry
 
 import shotgrid_leecher.mapper.hierarchy_mapper as mapper
 import shotgrid_leecher.utils.connectivity as conn
@@ -10,6 +13,36 @@ from shotgrid_leecher.utils.logger import get_logger
 from shotgrid_leecher.utils.timer import timed
 
 _LOG = get_logger(__name__.split(".")[-1])
+
+_EMPTY = f"EMPTY_{str(uuid.uuid4())}"
+_SEP = "."
+_star_select = curry(starmap)
+
+
+def _unpack_nested(key: str, value: Any) -> Iterable[Tuple[str, Any]]:
+    value_type = type(value)
+    if value_type is dict:
+        if not value:
+            yield key, _EMPTY
+        for k, v in value.items():
+            yield key + _SEP + k, v
+    if value_type is list:
+        if not value:
+            yield key, _EMPTY
+        for i, v in zip(range(len(value)), value):
+            yield key + _SEP + str(i), v
+    if value_type not in {list, dict}:
+        yield key, value
+
+
+def flatten_dict(dictionary: Dict[str, Any]) -> Dict[str, Any]:
+    dict_ = dictionary
+    while True:
+        dict_ = dict(
+            chain.from_iterable(starmap(_unpack_nested, dict_.items()))
+        )
+        if not any(type(x) in {list, dict} for x in dict_.values()):
+            return dict_
 
 
 class ShotgridHierarchyTraversal:
