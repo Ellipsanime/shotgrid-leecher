00806c6ab900989ff70780334454a960751c3a4e
Julien <julien.pierre.martin@gmail.com>
2021-09-15 18:32:17 +0200

    added (batch update) integration test on init db

--- tests/integration/test_batch.py
+++ tests/integration/test_batch.py
@@ -1,11 +1,88 @@
+import random
+import uuid
+from typing import Any, Callable, List
+from unittest.mock import Mock
 
-def test_update_shotgird_to_avalon_init():
+from _pytest.monkeypatch import MonkeyPatch
+from assertpy import assert_that
+from mongomock import MongoClient
+
+import shotgrid_leecher.utils.connectivity as conn
+import shotgrid_leecher.repository.shotgrid_hierarchy_repo as repository
+from shotgrid_leecher.domain import batch_domain as sut
+from shotgrid_leecher.record.commands import ShotgridToAvalonBatchCommand
+from shotgrid_leecher.record.shotgrid_structures import ShotgridCredentials
+
+TASK_NAMES = ["lines", "color", "look", "dev"]
+STEP_NAMES = ["modeling", "shading", "rigging"]
+
+
+def _fun(param: Any) -> Callable[[Any], Any]:
+    return lambda *_: param
+
+def _get_project():
+
+    project_id = str(uuid.uuid4())[0:8]
+    return {
+        "_id": f"Project_{project_id}",
+        "src_id": 111,
+        "type": "Project",
+        "parent": None,
+    }
+
+
+def _get_asset_group(project):
+
+    return {"_id": "Asset", "type": "Group", "parent": f",{project['_id']},"}
+
+
+def _get_shot_group(project):
+
+    return {"_id": "Shot", "type": "Group", "parent": f",{project['_id']},"}
+
+
+def _get_prp_asset(parent):
+    return [
+        {
+            "_id": "PRP",
+            "type": "Group",
+            "parent": f"{parent['parent']}{parent['_id']},",
+        },
+        {
+            "_id": "Fork",
+            "src_id": uuid.uuid4().int,
+            "type": "Asset",
+            "parent": f"{parent['parent']}{parent['_id']},PRP,",
+        },
+    ]
+
+
+def test_update_shotgrid_to_avalon_init(monkeypatch: MonkeyPatch):
+    # Arrange
+    client = MongoClient()
+    project = _get_project()
+    asset_grp = _get_asset_group(project)
+    data = [_get_project()] #[project, asset_grp, *_get_prp_asset(asset_grp)]
+
+    monkeypatch.setattr(repository, "get_hierarchy_by_project", _fun(data))
+    monkeypatch.setattr(conn, "get_db_client", _fun(client))
+    command = ShotgridToAvalonBatchCommand(
+        123, "", True, ShotgridCredentials("", "", "")
+    )
+
+    # Act
+    sut.batch_update_shotgrid_to_avalon(command)
+
+    # Assert
+    assert_that(client.list_database_names()).is_length(2)
+    assert_that(client.list_database_names()).is_equal_to(['shotgrid_openpype', 'avalon'])
+
     pass
 
 
-def test_update_shotgird_to_avalon_overwrite():
+def test_update_shotgrid_to_avalon_overwrite():
     pass
 
 
-def test_update_shotgird_to_avalon_update_values():
+def test_update_shotgrid_to_avalon_update_values():
     pass
